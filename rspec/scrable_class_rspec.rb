792abdf41d01e7076d5fef092c4a84b305d41e42
Lauren Granger <lr.granger21@gmail.com>
2015-10-13 15:37:20 -0700

    make directories and files

---
+++ rspec/scrable_class_rspec.rb
@@ -0,0 +1,3 @@
+
+
+# - A Spec file which corresponds to your `Scrabble` class. This spec should contain one "red" test as a starting point. (This test can be modified as your get further through the requirements)
